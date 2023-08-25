8c296cb059c6552e2e959dd16ef339969e0736a1
Aleksandrov Oleg <141931337+Ch1n-ch1nless@users.noreply.github.com>
2023-08-25 09:13:56 +0300

    Documentation v.1

---
+++ _solve_square_equation_8h.js
@@ -0,0 +1,9 @@
+var _solve_square_equation_8h =
+[
+    [ "CompareDouble", "_solve_square_equation_8h.html#a202e32e63779b54ac953c8282942f8b4", null ],
+    [ "IsZero", "_solve_square_equation_8h.html#adef6da3df72d6e1e028ba415233ae5d0", null ],
+    [ "MakeMinRoot1AndMaxRoot2", "_solve_square_equation_8h.html#a4ef02feb47ad8d0b41649886a13ee576", null ],
+    [ "SolveLinearEquation", "_solve_square_equation_8h.html#a71e14b6e81b2171ed8b3656f39333319", null ],
+    [ "SolveSquareEquation", "_solve_square_equation_8h.html#ab44524e4cb65cd2048f7cd262d0ba964", null ],
+    [ "EPS", "_solve_square_equation_8h.html#afe01386a82fedc016455a43c20280a97", null ]
+];
---
+++ annotated_dup.js
@@ -0,0 +1,5 @@
+var annotated_dup =
+[
+    [ "RootsOfTrinomial", "struct_roots_of_trinomial.html", "struct_roots_of_trinomial" ],
+    [ "SquareTrinomialCoef", "struct_square_trinomial_coef.html", "struct_square_trinomial_coef" ]
+];
